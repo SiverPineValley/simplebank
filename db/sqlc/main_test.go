ae3b719abc1779e1e6369e26a3c7c900bd36a6aa
Parkjongin <whddls9632@naver.com>
2022-10-16 21:24:28 +0900

    6. A Clean way to implement database transaction in Golang

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -18,7 +18,9 @@ const (
 	)
 
 func TestMain(m *testing.M) {
-	testDB, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
