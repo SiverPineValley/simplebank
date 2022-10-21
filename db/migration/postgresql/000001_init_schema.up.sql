8e5d76bc829a2acdfb720f74a4ea0098c71feb08
박종인 <whddls9632@hyundai-autoever.com>
2022-10-21 14:48:09 +0900

    postgresql 마이그레이션 파일 수정

--- db/migration/postgresql/000001_init_schema.up.sql
+++ db/migration/postgresql/000001_init_schema.up.sql
@@ -1,49 +1,48 @@
-CREATE TABLE accounts
+CREATE TABLE "accounts"
 (
-    id         serial PRIMARY KEY,
-    owner      varchar(100) NOT NULL,
-    balance    bigint       NOT NULL,
-    currency   varchar(100) NOT NULL,
-    created_at timestamp    NOT NULL DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "owner"    varchar NOT NULL,
+    "balance"  bigint  NOT NULL,
+    "currency" varchar NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE entries
+CREATE TABLE "entries"
 (
-    id         serial PRIMARY KEY,
-    account_id bigint    NOT NULL,
-    amount     bigint    NOT NULL COMMENT 'can be negative or positive',
-    created_at timestamp NOT NULL DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "account_id" bigint NOT NULL,
+    "amount"     bigint NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE TABLE transfers
+CREATE TABLE "transfers"
 (
-    id              serial PRIMARY KEY,
-    from_account_id bigint    NOT NULL,
-    to_account_id   bigint    NOT NULL,
-    amount          bigint    NOT NULL COMMENT 'must be positive',
-    created_at      timestamp NOT NULL DEFAULT (now())
+    "id" bigserial PRIMARY KEY,
+    "from_account_id" bigint NOT NULL,
+    "to_account_id"   bigint NOT NULL,
+    "amount"          bigint NOT NULL,
+    "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE entries
-    ADD FOREIGN KEY (account_id) REFERENCES accounts (id);
+ALTER TABLE "entries"
+    ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE transfers
-    ADD FOREIGN KEY (from_account_id) REFERENCES accounts (id);
+ALTER TABLE "transfers"
+    ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE transfers
-    ADD FOREIGN KEY (to_account_id) REFERENCES accounts (id);
+ALTER TABLE "transfers"
+    ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
 
-CREATE
-    INDEX index_accounts_owner ON accounts (owner);
+CREATE INDEX ON "accounts" ("owner");
 
-CREATE
-    INDEX index_entries_account_id ON entries (account_id);
+CREATE INDEX ON "entries" ("account_id");
 
-CREATE
-    INDEX index_transfers_from_account_id ON transfers (from_account_id);
+CREATE INDEX ON "transfers" ("from_account_id");
 
-CREATE
-    INDEX index_transfers_to_account_id ON transfers (to_account_id);
+CREATE INDEX ON "transfers" ("to_account_id");
 
-CREATE
-    INDEX index_transfers_from_to_account_id ON transfers (from_account_id, to_account_id);
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
+
+COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
